802f08c270f9a079a7ee779cfaa0283fa7de573b
Janis Stengrevics <janis.stengrevics@helmes.com>
2023-10-22 20:50:59 +0300

    Added OrderStatusChangedEventConsumer to test IConsumer<TEvent>. On every order status change send whatsapp message to customer

---
+++ src/Plugins/Nop.Plugin.RRAGoldFingers.WhatsApp/Order/Consumers/OrderStatusChangedEventConsumer.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Orders;
+using Nop.Plugin.Misc.Zettle.Services;
+using Nop.Plugin.RRAGoldFingers.WhatsApp.Models;
+using Nop.Services.Customers;
+using Nop.Services.Events;
+
+namespace Nop.Plugin.RRAGoldFingers.WhatsApp.Order.Consumers;
+
+public sealed class OrderStatusChangedEventConsumer : IConsumer<OrderStatusChangedEvent>
+{
+    private readonly WhatsappService _whatsapp;
+    private readonly ICustomerService _customerService;
+
+    public OrderStatusChangedEventConsumer(
+        WhatsappService whatsapp,
+        ICustomerService customerService)
+    {
+        _whatsapp = whatsapp;
+        _customerService = customerService;
+    }
+
+    public Task HandleEventAsync(OrderStatusChangedEvent eventMessage)
+    {
+        return SendWhatsappMessageToCustomer(eventMessage);
+    }
+
+    private async Task SendWhatsappMessageToCustomer(OrderStatusChangedEvent eventMessage)
+    {
+        var customer = await GetCustomer(eventMessage.Order.CustomerId);
+        var msg = BuildWhatsappMessageForCustomer(eventMessage, customer);
+        await _whatsapp.SendMessage(new ChatMessage(msg, "", customer.Phone));
+    }
+
+    private async Task<Customer> GetCustomer(int customerId)
+    {
+        var customer = await _customerService.GetCustomerByIdAsync(customerId);
+        return customer;
+    }
+
+    private static string BuildWhatsappMessageForCustomer(OrderStatusChangedEvent eventMessage, Customer customer)
+    {
+        var msg = $"Hi, {customer.FirstName}, your order ID '{eventMessage.Order.OrderGuid}' status '{eventMessage.PreviousOrderStatus}' has changed to '{eventMessage.Order.OrderStatus}'";
+        return msg;
+    }
+}
